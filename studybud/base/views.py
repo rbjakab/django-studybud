a24451a21992942f4b33a0c6fbf20309d49f89d8
rbjakab <jakabrichard23@gmail.com>
2023-02-01 00:47:54 +0100

    Add Login and Logout

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -11,6 +14,33 @@ from .forms import RoomForm
 #     {'id': 2, 'name': 'Design with me.'},
 #     {'id': 3, 'name': 'Frontend Developers'},
 # ]
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist.')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist.')
+
+    context = {}
+    return render(request, 'base/login_register.html', context=context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
