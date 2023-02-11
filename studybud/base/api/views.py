2ef3e08a16fc8892c6df8388501a541432988044
rbjakab <jakabrichard23@gmail.com>
2023-02-11 15:08:24 +0100

    Django REST Framework

--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -1,11 +1,27 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
 
 
-def getRoute(request):
+@api_view(['GET'])
+def getRoutes(request):
     routes = [
-        'GET /api',
         'GET /api/rooms',
         'GET /api/room/:id'
     ]
-    return Response(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
